831f2f7346724c4ebd9bbc1d51db669a97a24dbf
Alloky Alloky <alloky2011@gmail.com>
2019-11-11 01:08:45 +0300

    task_3

--- task_3/main.cpp
+++ task_3/main.cpp
@@ -7,12 +7,12 @@ public:
 	T value;
 };
 
-using ultimatePrinter = GenScatterHierarchy<TypeList<int, TypeList<bool, TypeList<double, NullType>>>, Holder>;
+using classHiracy = GenScatterHierarchy<TypeList<int, TypeList<bool, TypeList<double, NullType>>>, Holder>;
 
 
 void main() {
 
-	ultimatePrinter g;
+	classHiracy g;
 	get_value<int>(g).value = 42;
 	get_value<bool>(g).value = false;
 	get_value<double>(g).value = 2.3;
